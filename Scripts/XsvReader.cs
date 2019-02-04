66aed0c1b80571d07b6f2a076dd3796f49203c2a
coulank <kr.smn6vbg@gmail.com>
2019-02-04 10:34:59 +0900

    変更分の取り込み

--- Scripts/XsvReader.cs
+++ Scripts/XsvReader.cs
@@ -6,27 +6,28 @@ using UnityEngine;
 
 namespace Monry.XsvUtility
 {
-    // Primary key
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
-    public class XsvKeyAttribute : Attribute
-    {
-    }
     [Serializable]
     public class XsvReader
     {
         /// <summary>
         /// Most Prioritable Is Text Asset
         /// </summary>
-        [SerializeField]
-        public TextAsset m_CsvAsset;
-        [SerializeField]
-        public string m_CsvPathInResources;
-        [SerializeField]
+        [SerializeField, Tooltip("CSV or TSV Asset, Most Prioritable")]
+        public TextAsset m_XsvAsset;
+        [SerializeField, Tooltip("CSV or TSV File Path, then XsvAsset is null")]
+        public string m_XsvPathInResources;
+        [SerializeField, Tooltip("Header Row Skip Flag")]
         public bool m_HeaderEnable;
+        [SerializeField, Tooltip("Choose delimiter, CSV->Comma, TSV->Tab")]
+        public XsvParser.Delimiter m_Delimiter;
         public struct Data<T>
         {
             [XsvRow] public IEnumerable<T> Rows { get; set; }
         }
+        /// <summary>
+        /// カラムを解析して主キーを特定する、XsvKeyプロパティ指定で主キーとなる
+        /// Setting of Primary key from XsvKey Attribute Property
+        /// </summary>
         public static FieldInfo GetKeyColumn(Type type) {
             var fields = type
                     .GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
@@ -39,25 +40,41 @@ namespace Monry.XsvUtility
             }
             return ((keyList.Count > 0) ? keyList[0] : null);
         }
-        static public IEnumerable<T> ReadCsv<T>(TextAsset csvAsset = null, string csvPathInResources = "", bool headerEnable = false)
+        /// <summary>
+        /// XsvDeserializeを使ってパース開始
+        /// Run XsvDeserialize with options
+        /// </summary>
+        /// <typeparam name="T">Columns(Struct or Class) Type</typeparam>
+        static public IEnumerable<T> ReadXsv<T>(TextAsset xsvAsset, bool headerEnable = false,
+            XsvParser.Delimiter delimiter = XsvParser.Delimiter.Comma)
         {
-            if (csvAsset == null)
+            if (xsvAsset == null) return null;  // nullチェックは後々の処理を飛ばすために入れました
+            IEnumerable<T> deserialize;
+            switch (delimiter)
             {
-                csvAsset = Resources.Load(csvPathInResources) as TextAsset;
+                case XsvParser.Delimiter.Comma:
+                    deserialize = CsvSerializer.Deserialize<Data<T>>(xsvAsset.text).Rows;
+                    break;
+                case XsvParser.Delimiter.Tab:
+                    deserialize = TsvSerializer.Deserialize<Data<T>>(xsvAsset.text).Rows;
+                    break;
+                default:    // 例外処理はCSVとして処理します
+                    deserialize = CsvSerializer.Deserialize<Data<T>>(xsvAsset.text).Rows;
+                    break;
             }
-            if (csvAsset == null) return null;
-            var deserialize = CsvSerializer.Deserialize<Data<T>>(csvAsset.text).Rows;
             if (headerEnable) deserialize = deserialize.Skip(1);
             return deserialize;
         }
         /// <summary>
-        /// Get Directry Of Select KeyType Mode
+        /// 辞書型で取り出し
+        /// Get Directry of Select KeyType Mode from TextAsset
         /// </summary>
-        /// <typeparam name="T">Columns(Struct or Class) Type</typeparam>
         /// <typeparam name="K">Key Type</typeparam>
-        static public Dictionary<K, T> GetDictionary<K, T>(TextAsset csvAsset = null, string csvPathInResources = "", bool headerEnable = false)
+        /// <typeparam name="T">Columns(Struct or Class) Type</typeparam>
+        static public Dictionary<K, T> GetDictionary<K, T>(TextAsset xsvAsset, bool headerEnable = false,
+            XsvParser.Delimiter delimiter = XsvParser.Delimiter.Comma)
         {
-            var read = ReadCsv<T>(csvAsset, csvPathInResources, headerEnable);
+            var read = ReadXsv<T>(xsvAsset, headerEnable, delimiter);
             if (read == null) return null;
             return read
                 .GroupBy(val => {
@@ -72,28 +89,87 @@ namespace Monry.XsvUtility
                 }).ToDictionary(x => x.Key, x => x.Last());
         }
         /// <summary>
-        /// Get Directry Of String KeyType Mode
+        /// ファイルパスから辞書型で取り出し
+        /// Get Directry of Select KeyType Mode from ResourcesPath
+        /// </summary>
+        /// <typeparam name="K">Key Type</typeparam>
+        /// <typeparam name="T">Columns(Struct or Class) Type</typeparam>
+        /// <param name="xsvPathInResources">Resources file path</param>
+        /// <returns></returns>
+        static public Dictionary<K, T> GetDictionary<K, T>(string xsvPathInResources, bool headerEnable = false,
+            XsvParser.Delimiter delimiter = XsvParser.Delimiter.Comma)
+        {
+            return GetDictionary<K, T>(Resources.Load(xsvPathInResources) as TextAsset, headerEnable, delimiter);
+        }
+        /// <summary>
+        /// 文字列をキーとして辞書型で取り出し
+        /// Get Directry of String KeyType Mode
         /// </summary>
         /// <typeparam name="T">Columns(Struct or Class) Type</typeparam>
-        static public Dictionary<string, T> GetDictionary<T>(TextAsset csvAsset = null, string csvPathInResources = "", bool headerEnable = false)
+        static public Dictionary<string, T> GetDictionary<T>(TextAsset xsvAsset,
+            bool headerEnable = false, XsvParser.Delimiter delimiter = XsvParser.Delimiter.Comma)
         {
-            return GetDictionary<string, T>(csvAsset, csvPathInResources, headerEnable);
+            return GetDictionary<string, T>(xsvAsset, headerEnable, delimiter);
         }
+        /// <summary>
+        /// 文字列をキーとしてファイルパスから辞書型で取り出し
+        /// Get Directry of String KeyType Mode from ResourcesPath
+        /// </summary>
         /// <typeparam name="T">Columns(Struct or Class) Type</typeparam>
-        static public List<T> GetList<T>(TextAsset csvAsset = null, string csvPathInResources = "", bool headerEnable = false)
+        static public Dictionary<string, T> GetDictionary<T>(string xsvPathInResources,
+            bool headerEnable = false, XsvParser.Delimiter delimiter = XsvParser.Delimiter.Comma)
         {
-            return ReadCsv<T>(csvAsset, csvPathInResources, headerEnable).ToList();
+            return GetDictionary<string, T>(Resources.Load(xsvPathInResources) as TextAsset, headerEnable, delimiter);
         }
-        public Dictionary<string, T> ToDictionary<T>() {
-            return GetDictionary<string, T>(m_CsvAsset, m_CsvPathInResources, m_HeaderEnable);
+        /// <summary>
+        /// リスト型で取り出し
+        /// Get List from TextAsset
+        /// </summary>
+        /// <typeparam name="T">Columns(Struct or Class) Type</typeparam>
+        static public List<T> GetList<T>(TextAsset xsvAsset,
+            bool headerEnable = false, XsvParser.Delimiter delimiter = XsvParser.Delimiter.Comma)
+        {
+            return ReadXsv<T>(xsvAsset, headerEnable, delimiter).ToList();
         }
+        /// <summary>
+        /// ファイルパスからリスト型で取り出し
+        /// Get List from ResourcesPath
+        /// </summary>
+        /// <typeparam name="T">Columns(Struct or Class) Type</typeparam>
+        static public List<T> GetList<T>(string xsvPathInResources,
+            bool headerEnable = false, XsvParser.Delimiter delimiter = XsvParser.Delimiter.Comma)
+        {
+            return ReadXsv<T>(Resources.Load(xsvPathInResources) as TextAsset, headerEnable).ToList();
+        }
+        /// <summary>
+        /// メンバ変数から辞書型で取り出し
+        /// Get Directry of Select KeyType Mode from Member of TextAsset or Resources Filepath
+        /// </summary>
+        /// <typeparam name="K">Key Type</typeparam>
+        /// <typeparam name="T">Columns(Struct or Class) Type</typeparam>
         public Dictionary<K, T> ToDictionary<K, T>()
         {
-            return GetDictionary<K, T>(m_CsvAsset, m_CsvPathInResources, m_HeaderEnable);
+            if (m_XsvAsset == null) m_XsvAsset = Resources.Load<TextAsset>(m_XsvPathInResources);
+            return GetDictionary<K, T>(m_XsvAsset, m_HeaderEnable, m_Delimiter);
         }
+        /// <summary>
+        /// 文字列をキーとしてメンバ変数から辞書型で取り出し
+        /// Get Directry of String KeyType Mode from Member of TextAsset or Resources Filepath
+        /// </summary>
+        /// <typeparam name="T">Columns(Struct or Class) Type</typeparam>
+        public Dictionary<string, T> ToDictionary<T>() {
+            if (m_XsvAsset == null) m_XsvAsset = Resources.Load<TextAsset>(m_XsvPathInResources);
+            return GetDictionary<string, T>(m_XsvAsset, m_HeaderEnable, m_Delimiter);
+        }
+        /// <summary>
+        /// メンバ変数からリスト型で取り出し
+        /// Get List from Member of TextAsset or Resources Filepath
+        /// </summary>
+        /// <typeparam name="T">Columns(Struct or Class) Type</typeparam>
         public List<T> ToList<T>()
         {
-            return GetList<T>(m_CsvAsset, m_CsvPathInResources, m_HeaderEnable);
+            if (m_XsvAsset == null) m_XsvAsset = Resources.Load<TextAsset>(m_XsvPathInResources);
+            return GetList<T>(m_XsvAsset, m_HeaderEnable, m_Delimiter);
         }
     }
 }
